e8c1031d5ac7650403056b3f6c22681d385e0a69
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-05-19 16:35:43 -0300

    feat: criar e configurar AutoMapper e DependencyInjection

---
+++ Config/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Infrastructure.Interfaces;
+using Infrastructure.Repositories;
+
+namespace LivrariaApi.Config;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection DependenciesInjections(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAutorRepository, AutorRepository>();
+        services.AddScoped<IAutorService, AutorService>();
+
+        //services.AddScoped<ILivroRepository, LivroRepository>();
+        //services.AddScoped<ILivroService, LivroService>();
+
+        return services;
+    }
+}
