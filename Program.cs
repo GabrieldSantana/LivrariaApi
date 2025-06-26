7782dc2465617981f510a410676bd3930fbe81b2
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-06-26 15:04:53 -0300

    fix: ajustar Program

--- Program.cs
+++ Program.cs
@@ -1,19 +1,40 @@
+using System.Data;
 using LivrariaApi.Config;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddControllers();
+builder.Services.AddScoped<IDbConnection>(provider =>
+{
+    return new SqlConnection(connectionString);
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.DependenciesInjections(builder.Configuration);
+builder.Services.AddControllers();
+
+builder.Services.DependenciesInjections();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.Configure<RouteOptions>(options =>
 {
     options.LowercaseUrls = true; // Evita letras maiúsculas na url
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
 });
 
 var app = builder.Build();
