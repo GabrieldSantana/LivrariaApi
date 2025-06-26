4fb38674d3bf4188d4c76e9923fcb39f05331125
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-06-26 15:02:27 -0300

    feat: adicionar dependencias

--- Config/DependencyInjection.cs
+++ Config/DependencyInjection.cs
@@ -1,5 +1,10 @@
 ﻿using Application.Interfaces;
+using Application.Interfaces.IMainService;
+using Application.Interfaces.IMainService.IMainService;
 using Application.Services;
+using Application.Services.MainService;
+using Domain.Validators.AutorValidators;
+using Domain.Validators.LivroValidators;
 using Infrastructure.Interfaces;
 using Infrastructure.Repositories;
 
@@ -7,13 +12,21 @@ namespace LivrariaApi.Config;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection DependenciesInjections(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection DependenciesInjections(this IServiceCollection services)
     {
+        services.AddAutoMapper(typeof(Program).Assembly);
+
+        services.AddScoped<INotificador, Notificador>();
+
+        services.AddScoped<IMainService, MainService>();
+
         services.AddScoped<IAutorRepository, AutorRepository>();
         services.AddScoped<IAutorService, AutorService>();
+        services.AddScoped<AutorValidator>();
 
-        //services.AddScoped<ILivroRepository, LivroRepository>();
-        //services.AddScoped<ILivroService, LivroService>();
+        services.AddScoped<ILivroRepository, LivroRepository>();
+        services.AddScoped<ILivroService, LivroService>();
+        services.AddScoped<LivroValidator>();
 
         return services;
     }
